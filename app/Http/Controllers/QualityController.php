e19743818cc45fe38af0fa6fe0e1a98d3ee97c6a
ShreeKrishna <nitish.ghimire1@gmail.com>
2022-11-12 09:26:41 -0800

    remove the unwanted lines

--- app/Http/Controllers/QualityController.php
+++ app/Http/Controllers/QualityController.php
@@ -29,40 +29,27 @@ class QualityController extends Controller
     public function availableMovieCheck(Request $request){
         $client = new Client();
         $id = $request->id;
-        // dd($name);
-        // Go to the symfony.com website
         $crawler = $client->request('GET', 'https://www.2embed.to/embed/tmdb/movie?id=' . $id);
-        // dd($crawler);
-        // Get the latest post in this category and display the titles
+
         $message = $crawler->filter('h3.heading-large')->first()->text('Default text content');   
-        // print ($message->text('Default text content'));
         if($message != 'Default text content'){
-            // print ('movie not available');
             return 0;
         } else{
             return 1;
-            // print('movie available');
         }
-        // return '404 not found';
+        return '404 not found';
     }
 
     public function availableSerieCheck(Request $request){
         $client = new Client();
         $id = $request->id;
-        // dd($name);
-        // Go to the symfony.com website
         $crawler = $client->request('GET', 'https://www.2embed.to/embed/tmdb/tv?id=' . $id . '&s=1&e=1');
-        // dd($crawler);
-        // Get the latest post in this category and display the titles
         $message = $crawler->filter('h3.heading-large')->first()->text('Default text content');   
-        // print ($message->text('Default text content'));
         if($message != 'Default text content'){
-            // print ('movie not available');
             return 0;
         } else{
             return 1;
-            // print('movie available');
         }
-        // return '404 not found';
+        return '404 not found';
     }
 }
